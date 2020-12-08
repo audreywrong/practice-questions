ef505bedae0e8fd7080043d40e76b9bf8e4e7fe6
audreywrong <68037586+audreywrong@users.noreply.github.com>
2020-12-07 23:15:43 -0500

    added question 1

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,89 +1,92 @@
 var quizQuestions = [
   {
-      question: "What franchise would you rather play a game from?",
-      answers: [
-          {
-              type: "Microsoft",
-              content: "Halo"
-          },
-          {
-              type: "Nintendo",
-              content: "Pokemon"
-          },
-          {
-              type: "Sony",
-              content: "Uncharted"
-          }
-      ]
+    question: "What is grep in bash?",
+    answers: [
+      {
+        type: "Correct",
+        content:
+          "grep is a Linux / Unix command-line tool used to search for a string of characters in a specified file.",
+      },
+      {
+        type: "Incorrect",
+        content: "grep is a Grandpa's name.",
+      },
+      {
+        type: "Incorrect",
+        content:
+          "Slang form of 'gripe'. As in, 'To gripe about the laundry not being finished.' ",
+      },
+    ],
   },
   {
-      question: "Which console would you prefer to play with friends?",
-      answers: [
-          {
-              type: "Microsoft",
-              content: "X-Box"
-          },
-          {
-              type: "Nintendo",
-              content: "Nintendo 64"
-          },
-          {
-              type: "Sony",
-              content: "Playstation 1"
-          }
-      ]
+    question: "Which console would you prefer to play with friends?",
+    answers: [
+      {
+        type: "Microsoft",
+        content: "X-Box",
+      },
+      {
+        type: "Nintendo",
+        content: "Nintendo 64",
+      },
+      {
+        type: "Sony",
+        content: "Playstation 1",
+      },
+    ],
   },
   {
-      question: "Which of these racing franchises would you prefer to play a game from?",
-      answers: [
-          {
-              type: "Microsoft",
-              content: "Forza"
-          },
-          {
-              type: "Nintendo",
-              content: "Mario Kart"
-          },
-          {
-              type: "Sony",
-              content: "Gran Turismo"
-          }
-      ]
+    question:
+      "Which of these racing franchises would you prefer to play a game from?",
+    answers: [
+      {
+        type: "Microsoft",
+        content: "Forza",
+      },
+      {
+        type: "Nintendo",
+        content: "Mario Kart",
+      },
+      {
+        type: "Sony",
+        content: "Gran Turismo",
+      },
+    ],
   },
   {
-      question: "Which of these games do you think is best?",
-      answers: [
-          {
-              type: "Microsoft",
-              content: "BioShock"
-          },
-          {
-              type: "Nintendo",
-              content: "The Legend of Zelda: Ocarina of Time"
-          },
-          {
-              type: "Sony",
-              content: "Final Fantasy VII"
-          }
-      ]
+    question: "Which of these games do you think is best?",
+    answers: [
+      {
+        type: "Microsoft",
+        content: "BioShock",
+      },
+      {
+        type: "Nintendo",
+        content: "The Legend of Zelda: Ocarina of Time",
+      },
+      {
+        type: "Sony",
+        content: "Final Fantasy VII",
+      },
+    ],
   },
   {
-      question: "What console would you prefer to own?",
-      answers: [
-          {
-              type: "Microsoft",
-              content: "X-Box One"
-          },
-          {
-              type: "Nintendo",
-              content: "Wii U"
-          },
-          {
-              type: "Sony",
-              content: "Playstation 4"
-          }
-      ]
-  }
+    question: "What console would you prefer to own?",
+    answers: [
+      {
+        type: "Microsoft",
+        content: "X-Box One",
+      },
+      {
+        type: "Nintendo",
+        content: "Wii U",
+      },
+      {
+        type: "Sony",
+        content: "Playstation 4",
+      },
+    ],
+  },
 ];
 
 export default quizQuestions;
